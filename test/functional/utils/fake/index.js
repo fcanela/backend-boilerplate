6f9c2bcea095cbbf32df2d57fb1ecae35ad504c9
Francisco Canela <fcanela.dev@gmail.com>
2017-09-29 13:04:48 +0200

    WIP

--- test/functional/utils/fake/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const random = {
-  estate: require('./estate'),
-  number: require('./number'),
-};
-
-module.exports = random;
-
--- test/functional/utils/fake/number.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function generator(min=0, max=10000, precision=2) {
-  let number = Math.random() * (max - min) + min;
-  number = parseFloat(number.toFixed(precision));
-  return number;
-};
